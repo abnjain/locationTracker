fb86f8d0fea753b52220f31b720e7ddca79bb739
abnjain <abnjain25@gmail.com>
2024-07-04 23:48:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+    console.log("New user connected");
+    socket.on("send-location", (data) => {
+        io.emit("recieve-location", {id: socket.id, ...data});
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+server.listen(3000, () => {
+    console.log("Server connected");
+});
